24a72d874d0f39c861898d8d8d34c6ff36b49541
Dan Porter <dpreid@gmail.com>
2016-01-05 11:23:19 +0000

    Temporary Farset config

--- js/config.js
+++ js/config.js
@@ -1,30 +1,30 @@
 var config = {
-    lang: 'nl',
+    lang: 'en',
     time: {
-        timeFormat: 12
+        timeFormat: 24
     },
     weather: {
         //change weather params here:
         //units: metric or imperial
         params: {
-            q: 'Baarn,Netherlands',
+            q: 'Belfast,United Kingdom',
             units: 'metric',
             // if you want a different lang for the weather that what is set above, change it here
-            lang: 'nl',
-            APPID: 'YOUR_FREE_OPENWEATHER_API_KEY'
+            lang: 'en',
+            APPID: 'b5ca3e69f3053dcb5c8880e0915a2da0'
         }
     },
     compliments: {
         interval: 30000,
         fadeInterval: 4000,
         morning: [
-            'Good morning, handsome!',
+            'Good morning, Farset member!',
             'Enjoy your day!',
             'How was your sleep?'
         ],
         afternoon: [
-            'Hello, beauty!',
-            'You look sexy!',
+            'Afternoon, Farset member!',
+            'Welcome back!',
             'Looking good today!'
         ],
         evening: [
@@ -38,6 +38,6 @@ var config = {
         url: "https://p01-calendarws.icloud.com/ca/subscribe/1/n6x7Farxpt7m9S8bHg1TGArSj7J6kanm_2KEoJPL5YIAk3y70FpRo4GyWwO-6QfHSY5mXtHcRGVxYZUf7U3HPDOTG5x0qYnno1Zr_VuKH2M"
     },
     news: {
-        feed: 'http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml'
+        feed: 'http://blog.farsetlabs.org.uk/feed/'
     }
 }
